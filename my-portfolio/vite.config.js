af08306af6acfe28e5d151633b380ad1d1143788
kirankhant_11357 <keshavofficial57@gmail.com>
2025-11-22 10:42:35 +0530

    fix base path for github

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,5 +1,8 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/My_portfolio/', // 👈 must match your repo name
   plugins: [react()],
-});
+  base: '/My-portfolio/',   // 👈 important for GitHub Pages
+})
