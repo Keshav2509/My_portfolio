e7ba2be5a59985c307da4ee3c41b01adb79ce0ae
kirankhant_11357 <keshavofficial57@gmail.com>
2025-11-23 15:33:33 +0530

    Prepare for Vercel deployment

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
-import '../src/index.css'; // ✅ Make sure this file exists
+// main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
